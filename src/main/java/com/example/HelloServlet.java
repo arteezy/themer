9ce28c26c6ffe466f5df83ae2681d205a544a8b6
yeasayer <mardybm@gmail.com>
2012-07-13 02:22:20 +0700

    init

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -17,7 +17,7 @@ public class HelloServlet extends HttpServlet {
             throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
         
-        out.write("Hello Heroku".getBytes());
+        out.write("DEP-LOY-ED!".getBytes());
         out.flush();
         out.close();
     }
