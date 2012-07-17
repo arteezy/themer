634ea842cff58e5a825dd3dbd8a7f6bc0c3354e2
yeasayer <mardybm@gmail.com>
2012-07-18 03:01:50 +0700

    Added some annotations

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@SuppressWarnings("unused")
 public class HelloServlet extends HttpServlet {
 
 	@Override
