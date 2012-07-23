07facc362aa30f743fa5975107ebceeddd26b690
yeasayer <mardybm@gmail.com>
2012-07-24 04:33:21 +0700

    Added Stemming algorithm

--- src/main/java/com/art/Main.java
+++ src/main/java/com/art/Main.java
@@ -3,18 +3,8 @@ package com.art;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
 
-/**
- * 
- * This class launches the web application in an embedded Jetty container.
- * This is the entry point to your application. The Java command that is used for
- * launching should fire this main method.
- *
- */
-public class Main {
-    
-    /**
-     * @param args
-     */
+public class Main {    
+
     public static void main(String[] args) throws Exception{
         String webappDirLocation = "src/main/webapp/";
         
